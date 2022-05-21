2d31fc2889f748982a4a175b47a15d944a9fc129
sadikhossain69 <sadikhossain433@gmail.com>
2022-05-22 05:23:51 +0600

    login page designed

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -27,7 +27,7 @@ const Login = () => {
                             </div>
                             <div className="form-control mt-6">
                                 <button className="btn btn-primary">Login</button>
-                            </div>
+                            </div> 
                         </div>
                     </div>
                 </div>
