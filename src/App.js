bdb88d9579fba44e3912751758653e55bd2c98f0
sadikhossain69 <sadikhossain433@gmail.com>
2022-05-22 01:18:41 +0600

    react router setup and home page done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Pages/Home/Home/Home';
 import Navbar from './Pages/Shared/Navbar/Navbar';
+import PageNotFound from './Pages/Shared/PageNotFound/PageNotFound';
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='*' element={<PageNotFound/>} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/Shared/PageNotFound/PageNotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import error from '../../../Media/PageNotFound/error.png'
+
+const PageNotFound = () => {
+    return (
+        <div className='flex justify-center items-center w-full md:w-2/3 mx-auto flex-col'>
+            <img src={error} alt="" />
+            <Link className='bg-blue-500 text-white py-1 px-4 rounded hover:duration-300 hover:bg-blue-400' to='/'>Back To Home</Link>
+        </div>
+    );
+};
+
+export default PageNotFound;
