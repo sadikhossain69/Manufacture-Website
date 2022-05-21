f533353beaf67d96176c318184b09eb947a156e1
sadikhossain69 <sadikhossain433@gmail.com>
2022-05-22 00:23:42 +0600

    react router and daisyUi installed

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
       <h1 className="text-3xl font-bold underline text-center mt-56 text-red-600">
         Hello world!
       </h1>
+      <div className='text-center'>
+        <button className="btn btn-primary">Hello</button>
+      </div>
     </div>
   );
 }
