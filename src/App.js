c200fde5751e8c26269b2ad78e21885f51842e12
sadikhossain69 <sadikhossain433@gmail.com>
2022-05-22 00:53:24 +0600

    navbar designed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
+import Navbar from './Pages/Shared/Navbar/Navbar';
 
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline text-center mt-56 text-red-600">
-        Hello world!
-      </h1>
-      <div className='text-center'>
-        <button className="btn btn-primary">Hello</button>
-      </div>
+      <Navbar/>
     </div>
   );
 }
