3383505e2ab1e9f69786136d13a96e24b67754b3
sadikhossain69 <sadikhossain433@gmail.com>
2022-05-22 01:42:33 +0600

    banner part done

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
 
 const Home = () => {
     return (
-        <div>
-            <h2 className="text-3x">This is Home!!!</h2>
-        </div>
+        <main>
+            <Banner/>
+        </main>
     );
 };
 
