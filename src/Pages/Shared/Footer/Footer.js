0a11c05ea4c796b7d784d8d6a6d8403cb2119110
sadikhossain69 <sadikhossain433@gmail.com>
2022-05-22 03:02:37 +0600

    some issue fixed

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     const date = new Date().getFullYear()
 
     return (
-        <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
+        <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded mt-3">
             <div className="grid grid-flow-col gap-4">
                 <p className="link link-hover">About us</p>
                 <p className="link link-hover">Contact</p>
