62b22273e21348ad98027499e490c4f301d71326
sadikhossain69 <sadikhossain433@gmail.com>
2022-05-22 02:52:54 +0600

    our team section done

--- src/Pages/Home/OurTeam/OurTeam.js
+++ src/Pages/Home/OurTeam/OurTeam.js
@@ -24,7 +24,7 @@ const OurTeam = () => {
                         </div>
                     </div>
                     <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
-                        <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                        <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg"> 
                             <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4 ring-2" src={people1}/>
                                 <div class="flex-grow">
                                     <h2 class="text-gray-900 title-font font-medium">Henry Letham</h2>
