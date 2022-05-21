e0c5d6e5c173f9ee8070f83096d4b13f5c091eda
sadikhossain69 <sadikhossain433@gmail.com>
2022-05-22 04:19:17 +0600

    login page desinged

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home/Home/Home';
+import Login from './Pages/Login/Login';
 import Footer from './Pages/Shared/Footer/Footer';
 import Navbar from './Pages/Shared/Navbar/Navbar';
 import PageNotFound from './Pages/Shared/PageNotFound/PageNotFound';
@@ -11,6 +12,7 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>} />
+        <Route path='/login' element={<Login/>} />
         <Route path='*' element={<PageNotFound/>} />
       </Routes>
       <Footer/>
---
+++ src/Pages/Login/Login.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import login from '../../Media/Login/login.png'
+
+const Login = () => {
+    return (
+        <section>
+            <h2 className="text-3xl text-center mt-3 underline underline-offset-8 text-accent decoration-accent">Login Here</h2>
+            <div className="hero min-h-screen">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <img className='w-96 ml-9' src={login} alt="" />
+                    <div className="card flex-shrink-0 w-full lg:w-96 shadow-2xl bg-base-100">
+                        <div className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="text" placeholder="email" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="text" placeholder="password" className="input input-bordered" />
+                                <label className="label">
+                                    <button href="#" className="label-text-alt link link-hover">Forgot password?</button>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Login</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Login;
