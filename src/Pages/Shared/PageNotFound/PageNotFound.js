a57faa08eeb33fda281808c35f7a424d89757a45
sadikhossain69 <sadikhossain433@gmail.com>
2022-05-22 01:19:10 +0600

    react router setup and 404 page done

--- src/Pages/Shared/PageNotFound/PageNotFound.js
+++ src/Pages/Shared/PageNotFound/PageNotFound.js
@@ -7,7 +7,7 @@ const PageNotFound = () => {
         <div className='flex justify-center items-center w-full md:w-2/3 mx-auto flex-col'>
             <img src={error} alt="" />
             <Link className='bg-blue-500 text-white py-1 px-4 rounded hover:duration-300 hover:bg-blue-400' to='/'>Back To Home</Link>
-        </div>
+        </div> 
     );
 };
 
