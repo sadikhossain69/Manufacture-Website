1eebfbf3e2cc8cc7f7353508f419e95a0191eb89
sadikhossain69 <sadikhossain433@gmail.com>
2022-05-25 21:13:17 +0600

    make admin feature added

--- src/Pages/Dashboard/UserRow/UserRow.js
+++ src/Pages/Dashboard/UserRow/UserRow.js
@@ -1,11 +1,43 @@
 import React from 'react';
+import toast from 'react-hot-toast';
 
 const UserRow = ({ user, refetch, index }) => {
+
+
+    const makeAdmin = () => {
+        fetch(`http://localhost:5000/user/admin/${user.email}`, {
+            method: 'PUT',
+            headers: {
+                authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+            .then(res => {
+                if(res.status === 403) {
+                    toast.error('Failed to make an Admin', {id: 'failed to make an admin'})
+                }
+                return res.json()
+            })
+            .then(data => {
+                if (data.modifiedCount > 0) {
+                    refetch()
+                    console.log(data);
+                    return toast.success("Successfully make Admin", { id: 'admin toast' })
+                }
+            })
+    }
+
     return (
         <tr className="hover">
             <th>{index + 1}</th>
             <td>{user.email}</td>
-            <td><button className='btn btn-xs'>Make Admin</button></td>
+            <td>
+                {
+                    user.role !== 'admin' ?
+                    <button onClick={makeAdmin} className='btn btn-xs'>Make Admin</button>
+                    :
+                    <button className="btn btn-xs bg-green-600 border-0 hover:bg-green-500">Already Admin</button>
+                }
+            </td>
         </tr>
     );
 };
