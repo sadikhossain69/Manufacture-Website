e287226462adc6da977503d4128b107252b30244
sadikhossain69 <sadikhossain433@gmail.com>
2022-05-26 06:05:56 +0600

    now admin can delete tool

--- src/Pages/Dashboard/ToolsDeletingModal/ToolsDeletingModal.js
+++ src/Pages/Dashboard/ToolsDeletingModal/ToolsDeletingModal.js
@@ -1,6 +1,33 @@
 import React from 'react';
+import toast from 'react-hot-toast';
 
 const ToolsDeletingModal = ({ toolDelete, setToolDelete, refetch }) => {
+
+    const id = toolDelete._id
+    console.log(id);
+
+    const handleToolDelete = () => {
+        const url = `http://localhost:5000/tools/${id}`
+        fetch(url, {
+            method: "DELETE",
+            headers: {
+                authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            if (data.deletedCount) {
+                toast.success("Tool Deleted Successfully", { id: 'Tool Deleted Successfully' })
+                setToolDelete(null)
+                refetch()
+            }
+            else {
+                toast.error('Failed to Delete Tool', { id: "Failed to Delete Tool" })
+            }
+        })
+    }
+
     return (
         <section>
             <input type="checkbox" id="my-modal-3" className="modal-toggle" />
@@ -9,7 +36,7 @@ const ToolsDeletingModal = ({ toolDelete, setToolDelete, refetch }) => {
                     <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
                     <h3 className="text-lg font-bold">Are You Sure, You Want To Delete <span className='text-green-500'>{toolDelete.toolName}</span> Tool From Your Tool Collection?</h3>
                     <div className="text-right mt-2">
-                        <button className='bg-red-600 text-white py-1 px-4 duration-300 cursor-pointer hover:bg-red-500 rounded font-semibold hover:scale-110 ease-in-out'>Confirm</button>
+                        <button onClick={handleToolDelete} className='bg-red-600 text-white py-1 px-4 duration-300 cursor-pointer hover:bg-red-500 rounded font-semibold hover:scale-110 ease-in-out'>Confirm</button>
                     </div>
                 </div>
             </div>
